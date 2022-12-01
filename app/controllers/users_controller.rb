fc4c7712259d1f29e8a3d38587ce109bf4415eaf
Ueta0709 <113412485+Ueta0709@users.noreply.github.com>
2022-12-01 22:29:58 +0900

    エラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,10 @@
 class UsersController < ApplicationController
 
   def show
-    prototype = Prototype.find(params[:id])
-    @name = prototype.user.name
-    @profile = prototype.user.profile
-    @occupation = prototype.user.occupation
-    @position = prototype.user.position
+    @prototype = Prototype.find(params[:id])
     @user = User.find(params[:id])
     @prototypes = @user.prototypes.includes(:user)
+  
   end
 
 end
