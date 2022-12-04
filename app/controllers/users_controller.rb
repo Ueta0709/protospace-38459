9808aa6eb73b74a6eb54172e238c9d1988687727
Ueta0709 <113412485+Ueta0709@users.noreply.github.com>
2022-12-04 21:12:48 +0900

    エラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,8 @@
 class UsersController < ApplicationController
 
   def show
-    @prototype = Prototype.find(params[:id])
     @user = User.find(params[:id])
-    @prototypes = @user.prototypes.includes(:user)
-  
+    @prototypes = @user.prototypes.includes(:user)  
   end
 
 end
